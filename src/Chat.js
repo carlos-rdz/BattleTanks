d4fc37e2852b28c8bb4fb9af8db6bce306aead3f
Michael Blatchford <mblatchford@mac.com>
2019-01-12 10:56:14 -0500

    display message history

--- src/Chat.js
+++ src/Chat.js
@@ -6,25 +6,35 @@ class Chat extends Component {
     this.state = {
       chat: [],
       name: '', 
-      value: '',
+      message: '',
     }
   }
+  //put message into state and call helper function
   _handleChange = (e) => {
     this.setState({
-      value: e.target.value,
-    });
+      message: e.target.value
+    })
+  }
+  
+  // copy chat arr add new message, set state to updated arr
+  _addToChat = () => {
+    let chatHistory = this.state.chat;
+    chatHistory.push(this.state.message)
+    this.setState({
+      chat: chatHistory 
+    })
   }
 
-  _handleChangeName = (e) => {
-    this.setState({
-      name: e.target.value
-    });
-  }
+  // _handleChangeName = (e) => {
+  //   this.setState({
+  //     name: e.target.value
+  //   });
+  // }
 
   _handleSubmit = (e) => {
     e.preventDefault();
     console.log("message submitted")
-    
+    this._addToChat()
   }
 
   render() {
@@ -32,20 +42,20 @@ class Chat extends Component {
       <div>
         {/* return both an input form and a chat history div */}
         <form onSubmit={this._handleSubmit}>
-          <input type='text' 
+          {/* <input type='text' 
             name={this.state.name} 
             onChange={this._handleChangeName}>
-          </input>
+          </input> */}
           <input type='text' 
             placeholder='(Clean!) SmackTalk anyone???' 
-            value={this.state.value} 
+            value={this.state.message} 
             onChange={this._handleChange} >
           </input>
           <input type='submit' value='Send'></input>
 
         </form>
         <div className='chatMessages'>
-          messages go here
+          {this.state.chat}
         </div>
       </div>
     );
