aa8053a2f54c818d070e6f55660217e24541bd21
Michael Blatchford <mblatchford@mac.com>
2019-01-14 12:52:31 -0500

    Pushed Title.js to github

---
+++ src/Title.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className='gameTitle'>
+      <div style={{
+        color: 'red', 					
+        fontFamily: 'Contrail One, cursive',
+        fontSize: '4em',
+      }}>
+        Battle
+        </div>
+      <div style={{
+        color: 'blue', 					
+        fontFamily: 'Contrail One, cursive',
+        fontSize: '4em',
+      }}>
+      Tanks
+      </div>
+    </div>
+  );
+};
+
+export default Title;
