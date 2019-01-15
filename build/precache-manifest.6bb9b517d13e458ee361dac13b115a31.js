c47da77623691ad07961165cc7b6e5716ae26bc2
Michael Blatchford <mblatchford@mac.com>
2019-01-15 11:44:24 -0500

    add build folder

---
+++ build/precache-manifest.6bb9b517d13e458ee361dac13b115a31.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f44150f6644bbce4537f16c454ca1710",
+    "url": "/static/media/explosion1.f44150f6.wav"
+  },
+  {
+    "revision": "0cea439ccffdc8ac26b7",
+    "url": "/static/css/main.0fc83623.chunk.css"
+  },
+  {
+    "revision": "6e6acdba3ba0052ef33e",
+    "url": "/static/js/1.6e6acdba.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e2d591268ec492a3002bd89cd6b1336e",
+    "url": "/static/media/firecombined.e2d59126.gif"
+  },
+  {
+    "revision": "a835e1c17f0f1699ee7e74fcb7915a4f",
+    "url": "/static/media/misscombined.a835e1c1.gif"
+  },
+  {
+    "revision": "0cea439ccffdc8ac26b7",
+    "url": "/static/js/main.0cea439c.chunk.js"
+  },
+  {
+    "revision": "985d0fb9bb05acac2275ee5a420bf79a",
+    "url": "/static/media/explosion2.985d0fb9.wav"
+  },
+  {
+    "revision": "9edaa7eda73a06ddd41018a244c4704a",
+    "url": "/static/media/explosion3.9edaa7ed.wav"
+  },
+  {
+    "revision": "6112ef4a38b5817c9d1225798c56430d",
+    "url": "/static/media/firecombined.6112ef4a.wav"
+  },
+  {
+    "revision": "9d9359046efa4d81e73141ed43c1ec6c",
+    "url": "/static/media/misscombined.9d935904.wav"
+  },
+  {
+    "revision": "ca43b88dd83d5d48c1047de3e470aa7f",
+    "url": "/index.html"
+  }
+];
