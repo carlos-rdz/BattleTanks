a19bddc7e0c3b6a9a84c551f86523be5cf46ac1b
Michael Blatchford <mblatchford@mac.com>
2019-01-16 11:13:08 -0500

    Replaced assets mistakenly removed

---
+++ src/HitPlayerBoard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HitPlayerBoard from './Assets/hitNoStaticBlank.gif';
+
+const onHit = () => {
+	return (
+		<div>
+			<img src={HitPlayerBoard} alt='' style={{ width: 75+'px', height: 75+'px'}} />
+		</div>
+	);
+};
+
+export default onHit;
